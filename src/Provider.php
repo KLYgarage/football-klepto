dadd2fb574499cd35169094dadcfa69f2ce25b68
kenny <kennykarnama@gmail.com>
2018-07-26 12:43:46 +0700

    give docs

--- src/Provider.php
+++ src/Provider.php
@@ -3,17 +3,28 @@
 namespace Football;
 
 /**
- *
+ * Class to interact with
+ * Football API
  */
 class Provider
 {
+    /**
+     * Api key
+     * @var string
+     */
     private $apiKey;
-    
+    /**
+     * Constructor
+     * @param string $apiKey
+     */
     public function __construct($apiKey = '')
     {
         $this->apiKey = $apiKey;
     }
-
+    /**
+     * Get client api key
+     * @return string
+     */
     public function getApiKey()
     {
         return $this->apiKey;
